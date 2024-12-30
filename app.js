850d52a8a27475075bc78135a44032639a1a3c88
Antonio Cunanan <antonio.cunanan@gmail.com>
2024-12-30 01:19:49 -0600

    Update trials

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const parsedData = JSON.parse(data)
 // Create a separate file for each object in the array
 parsedData.forEach((item, index) => {
 	fs.writeFileSync(
-		`./trials/trial-${index + 1}.txt`,
+		`./trials/trial-${index + 1}.json`,
 		JSON.stringify(item, null, 2),
 		{ flag: 'w' } // 'w' - Open file for writing. The file is created if it does not exist, or truncated if it exists.
 	)
